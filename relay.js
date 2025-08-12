6f7296af08a47cb21236d382d2d7eebe5efae526
Michael Eden <70781525+Deltt@users.noreply.github.com>
2025-08-12 02:54:55 +0200

    Update

--- relay.js
+++ relay.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3000;
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: PORT });
 
-const lobbies = new Set();
+const lobbies = new Map();
 
 wss.on("connection", (ws) => {
     console.log("Client connected");
@@ -16,15 +16,40 @@ wss.on("connection", (ws) => {
     //     });
     // });
 
-	ws.on("message", (msg) => {
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(msg);
-            }
-        });
+	ws.on("message", (msg, isBinary) => {
+        if (!isBinary) {
+			console.log(msg);
+			return;
+		}
+
+		const buffer = message;
+		let offset = 0;
+
+		const commandId = buffer.readInt32LE(offset);
+		offset += 4;
+
+		if (commandId == 0)
+		{
+			const maxPlayers = buffer.readInt32LE(offset);
+			offset += 4;
+
+			const nameLength = buffer.readInt32LE(offset);
+			offset += 4;
+
+			const lobbyName = buffer.toString('utf8', offset, offset + nameLength);
+			offset += nameLength;
+
+			lobbies.set(ws, [maxPlayers, lobbyName])
+		}
+		
     });
 
     ws.on("close", () => {
         console.log("Client disconnected");
+
+		if (lobbies.has(ws))
+		{
+			lobbies.delete(ws)
+		}
     });
 });
